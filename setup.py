a252013081195f79f684fe80fc42402db932e464
susantkumarsahoo <susantkumar.sks96@gmail.com>
2025-09-08 14:14:48 +0530

    file uplode

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="StudentPerformancePrediction",
+    version="0.1.0",
+    author="Susant Kumar",
+    author_email="susantkumar.sks96@gmail.com",
+    description="End-to-End Student Performance Prediction project with ML and Flask.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    # url="https://github.com/yourusername/StudentPerformancePrediction-End-To-End",  # optional
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "seaborn",
+        "matplotlib",
+        "scikit-learn",
+        "xgboost",
+        "Flask",
+        "from_root"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",   # change if you use a different license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    include_package_data=True,
+)
