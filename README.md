6fb296c9d721e51cb9be0bdace462da1a8e72f90
susantkumarsahoo <susantkumar.sks96@gmail.com>
2025-09-20 05:01:15 +0530

    uplode

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # StudentPerformancePrediction-End-To-End
 Student performance prediction uses machine learning and analytics to forecast a student's academic success by analyzing historical and behavioral data like grades, attendance, and engagement.
+
+
+
+
+
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,27 +1,3 @@
-'''
-from src.entity.config_entity import DataIngestionConfig
-from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataValidationConfig
-from src.components.data_validation import DataValidation
-
-
-class TrainingPipeline:
-    def __init__(self, dataset_path: str):
-        self.dataset_path = dataset_path
-
-    def run_pipeline(self):
-        # Step 1: Data Ingestion
-        ingestion_config = DataIngestionConfig(dataset_path=self.dataset_path)
-        ingestion = DataIngestion(ingestion_config)
-        ingestion_artifact = ingestion.initiate_data_ingestion()
-
-
-        # Step 1: Data Ingestion
-        datavalidation_config = DataValidationConfig(dataset_path=self.dataset_path)
-        datavalidation = DataValidation(datavalidation_config)
-        datavalidation_artifact = datavalidation.initiate_data_validation()
-        '''
-
 from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
